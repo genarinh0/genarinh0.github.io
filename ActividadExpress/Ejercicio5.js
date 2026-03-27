db10a356db429b8eaa53b2859516b2d4b90ee544
Genaro <genaromercado101@gmail.com>
2026-03-26 19:12:44 -0600

    Express

---
+++ ActividadExpress/Ejercicio5.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+// Necesario para recrear __dirname en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get('/bienvenida', (req, res) => {
+  res.send('Esto no es una página html');
+});
+
+app.get('/otraBienvenida', (req, res) => {
+  res.sendFile(path.join(__dirname, 'bienvenida.html'));
+});
+
+app.listen(1984, () => {
+  console.log('Up and up');
+});
